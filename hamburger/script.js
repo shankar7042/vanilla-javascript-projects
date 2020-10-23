db25d3fc7672b3339e4e336cc808f2b1372625ff
Shankar <shankar01@ad.infosys.com>
2020-10-23 16:12:41 +0530

    hamburger navigation

---
+++ hamburger/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
